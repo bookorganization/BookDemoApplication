a8cfb411f30c819ed367516bc9711257031f28a6
juri <804454576@qq.com>
2019-03-20 10:47:42 +0800

    home.js 添加传数据测试 提交了原来版本的代码

--- app/src/main/assets/src/js/home.js
+++ app/src/main/assets/src/js/home.js
@@ -1,9 +1,69 @@
-var swiper = new Swiper('.swiper-container', {
-    pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,        
-    },
-    autoplay: {
-        delay: 2000,
-    },
-});
+$(document).ready(function(){
+
+    // 轮播图加载
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,        
+        },
+        autoplay: {
+            delay: 2000,
+        },
+    });
+
+    // 点击图书块传数据
+    $('.book-box').on('click',function(event){
+        var sendAddr = $(this).attr('href')
+        // $('.book-title').text('repsonseData from java, data =!!!!!!!!!!!!!!!!')
+        event.preventDefault()
+        window.WebViewJavascriptBridge.callHandler(
+            "javaWebViewHandler",
+            sendAddr,
+            function(responseData) {
+                $('.book-title').text('repsonseData from java, data =!!!!!!!!!!!!!!!!')
+                console.log("repsonseData from java, data =!!!!!!!!!!!!!!!! ")
+            }
+        );
+    })
+    
+
+    // js bridge
+    function connectWebViewJavascriptBridge(callback) {
+        if (window.WebViewJavascriptBridge) {
+            callback(WebViewJavascriptBridge)
+        } else {
+            document.addEventListener(
+                'WebViewJavascriptBridgeReady'
+                , function() {
+                    callback(WebViewJavascriptBridge)
+                },
+                false
+            );
+        }
+    }
+
+    
+    
+    connectWebViewJavascriptBridge(function(bridge) {
+        bridge.init(function(message, responseCallback) {
+            console.log('JS got a message', message);
+            var data = {
+                'Javascript Responds': '测试中文!'
+            };
+    
+            if (responseCallback) {
+                console.log('JS responding with', data);
+                responseCallback(data);
+            }
+        });
+    
+        bridge.registerHandler("functionJs", function(data, responseCallback) {
+            // document.getElementById("show").innerHTML = ("data from Java: = " + data);
+            if (responseCallback) {
+                var responseData = "java端调用js代码成功Javascript Says Right back aka!";
+                responseCallback(responseData);
+            }
+        });
+    })
+
+})
