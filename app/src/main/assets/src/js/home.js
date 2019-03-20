900a2e536db14249081bf737c223d3725a67bdf9
shenyueyue <1013250411@qq.com>
2019-03-20 20:01:25 +0800

    数据传输1

--- app/src/main/assets/src/js/home.js
+++ app/src/main/assets/src/js/home.js
@@ -12,58 +12,93 @@ $(document).ready(function(){
     });
 
     // 点击图书块传数据
+
+
     $('.book-box').on('click',function(event){
         var sendAddr = $(this).attr('href')
-        // $('.book-title').text('repsonseData from java, data =!!!!!!!!!!!!!!!!')
+
         event.preventDefault()
-        window.WebViewJavascriptBridge.callHandler(
-            "javaWebViewHandler",
-            sendAddr,
-            function(responseData) {
-                $('.book-title').text('repsonseData from java, data =!!!!!!!!!!!!!!!!')
-                console.log("repsonseData from java, data =!!!!!!!!!!!!!!!! ")
-            }
-        );
+       window.WebViewJavascriptBridge.callHandler(
+                     'submitFromWeb'
+                     , {'param': '中文测试'}
+                     , function(responseData) {
+                         document.getElementById("show").innerHTML = "send get responseData from java, data = " + responseData
+                     }
+                 );
     })
-    
+
 
     // js bridge
-    function connectWebViewJavascriptBridge(callback) {
-        if (window.WebViewJavascriptBridge) {
-            callback(WebViewJavascriptBridge)
-        } else {
-            document.addEventListener(
-                'WebViewJavascriptBridgeReady'
-                , function() {
-                    callback(WebViewJavascriptBridge)
-                },
-                false
+      function testDiv() {
+            document.getElementById("show").innerHTML = document.getElementsByTagName("html")[0].innerHTML;
+        }
+
+        function testClick() {
+            var str1 = document.getElementById("text1").value;
+            var str2 = document.getElementById("text2").value;
+
+            //send message to native
+            var data = {id: 1, content: "这是一个图片 "};
+            window.WebViewJavascriptBridge.send(
+                data
+                , function(responseData) {
+                    document.getElementById("show").innerHTML = "repsonseData from java, data =!!!!!!!!!!!!!!!! " + responseData
+                }
+            );
+
+        }
+
+        function testClick1() {
+            var str1 = document.getElementById("text1").value;
+            var str2 = document.getElementById("text2").value;
+
+            //call native method
+            window.WebViewJavascriptBridge.callHandler(
+                'submitFromWeb'
+                , {'param': '中文测试'}
+                , function(responseData) {
+                    document.getElementById("show").innerHTML = "send get responseData from java, data = " + responseData
+                }
             );
         }
-    }
 
-    
-    
-    connectWebViewJavascriptBridge(function(bridge) {
-        bridge.init(function(message, responseCallback) {
-            console.log('JS got a message', message);
-            var data = {
-                'Javascript Responds': '测试中文!'
-            };
-    
-            if (responseCallback) {
-                console.log('JS responding with', data);
-                responseCallback(data);
-            }
-        });
-    
-        bridge.registerHandler("functionJs", function(data, responseCallback) {
-            // document.getElementById("show").innerHTML = ("data from Java: = " + data);
-            if (responseCallback) {
-                var responseData = "java端调用js代码成功Javascript Says Right back aka!";
-                responseCallback(responseData);
-            }
-        });
-    })
+        function bridgeLog(logContent) {
+            document.getElementById("show").innerHTML = logContent;
+        }
 
+        function connectWebViewJavascriptBridge(callback) {
+            if (window.WebViewJavascriptBridge) {
+                callback(WebViewJavascriptBridge)
+            } else {
+                document.addEventListener(
+                    'WebViewJavascriptBridgeReady'
+                    , function() {
+                        callback(WebViewJavascriptBridge)
+                    },
+                    false
+                );
+            }
+        }
+
+        connectWebViewJavascriptBridge(function(bridge) {
+            bridge.init(function(message, responseCallback) {
+                console.log('JS got a message', message);
+                var data = {
+                    'Javascript Responds': '测试中文!'
+                };
+
+                if (responseCallback) {
+                    console.log('JS responding with', data);
+                    responseCallback(data);
+                }
+            });
+
+            bridge.registerHandler("functionInJs", function(data, responseCallback) {
+                document.getElementById("show").innerHTML = ("data from Java: = " + data);
+                if (responseCallback) {
+                    var responseData = "java端调用js代码成功Javascript Says Right back aka!";
+                    responseCallback(responseData);
+                }
+            });
+        })
 })
