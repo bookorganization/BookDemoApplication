9770cd09d0f084012e4aa4ecb732674631c99c96
juri <804454576@qq.com>
2019-03-20 10:31:50 +0800

    book.html

--- app/src/main/assets/src/js/book.js
+++ app/src/main/assets/src/js/book.js
@@ -1,8 +1,23 @@
 const dp = new DPlayer({
     container: document.getElementById('dplayer'),
     video: {
-        url: '../src/video/beautiful.mp4',
-        pic: '../src/img/c.png',
-        thumbnails: '../src/img/c.png'
+        url: 'http://media.qingzaodushu.com/%E5%A4%8F%E6%B4%9B%E7%9A%84%E7%BD%91-%E9%9D%92%E6%9E%A3%E8%AF%BB%E4%B9%A6.mp4',
+        // pic: '../src/img/c.png',
+        // thumbnails: '../src/img/c.png'
     },     
 });
+
+var bookApp = new Vue({
+    el:'#book-part',
+    data:{
+        bookTabShow : 1,
+    },
+    methods:{
+        tabShow:function(index){
+            console.log('xixi')
+            this.bookTabShow=index;
+            console.log(this.bookTabShow)
+        }
+    }
+
+})
