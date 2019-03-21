5ae35f129f266ef90f780e3130202ff439765d4d
juri <804454576@qq.com>
2019-03-21 19:16:37 +0800

    home.js 删除jsbridge

--- app/src/main/assets/src/js/booklist.js
+++ app/src/main/assets/src/js/booklist.js
@@ -4,7 +4,7 @@ var bookListHome = new Vue({
     data:{
         booklist:[
             {
-                id: '1',
+                id: '1', 
                 cover: '../src/img/b1.png',
                 title: "青鸟",
                 short: '当孩子想知道幸福是什么，不妨听听《青鸟》的答案。',
@@ -16,7 +16,7 @@ var bookListHome = new Vue({
             }, {
                 id: '1',
                 cover: '../src/img/b1.png',
-                title: "青鸟",
+                title: "青鸟34",
                 short: '当孩子想知道幸福是什么，不妨听听《青鸟》的答案。',
                 type: '儿童文学',
                 playtimes: '2309',
