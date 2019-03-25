a6fdee11a9a70b590692a13cc6fce878edc640fa
juri <804454576@qq.com>
2019-03-25 16:40:04 +0800

    classes

--- app/src/main/assets/src/js/book.js
+++ app/src/main/assets/src/js/book.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
         },
     });
 
+    // 切换选项卡
     var bookApp = new Vue({
         el:'#book-part',
         data:{
@@ -15,12 +16,58 @@ $(document).ready(function(){
         },
         methods:{
             tabShow:function(index){
-                console.log('xixi')
                 this.bookTabShow=index;
                 console.log(this.bookTabShow)
             }
         }
 
     })
+
+    // qrcode
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: "https://www.baidu.com/",
+        width: 128,
+        height: 128,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.L
+    });
+
+    // 音频切换
+    $('.bt2').on('click',function(){
+        audioSwitch()
+    })
+    function audioSwitch(){
+        window.location = "audio.html";
+    }
+
+    //滑动切换
+    // $('#book-part').touchwipe({
+    //     min_move_x : 20, // 横向灵敏度
+    //     min_move_y : 20, // 纵向灵敏度
+          
+    //     wipeLeft : function() {
+    //         console.log("向左滑动了");
+    //         if(bookApp.$data.bookTabShow < 3){
+    //             bookApp.$data.bookTabShow ++
+    //         }else{
+    //             bookApp.$data.bookTabShow = 3
+    //         }            
+    //         console.log(bookApp.$data.bookTabShow)
+              
+    //     },
+    //     wipeRight : function() {
+    //         if(bookApp.$data.bookTabShow > 1){
+    //             bookApp.$data.bookTabShow --
+    //         }else{
+    //             bookApp.$data.bookTabShow = 1
+    //         }            
+    //         console.log(bookApp.$data.bookTabShow)
+    //         console.log("向右滑动了");
+              
+    //     },
+    // });
+
+    
 
 })
